edc7a905874798ee12f34793a766c2b59115334d
mdinos <32902031+mdinos@users.noreply.github.com>
2019-07-31 15:40:46 +0100

    wip

--- two-nums-as-linked-list/main.py
+++ two-nums-as-linked-list/main.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 class ListNode(object):
     def __init__(self, x):
         self.val = x
@@ -7,8 +9,13 @@ class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode, c = 0):
         self.l1 = self._generate_integer(l1)
         self.l2 = self._generate_integer(l2)
-        self.total = self.l1 + self.l2
+        print('l1: {}  l2: {}'.format(self.l1, self.l2))
+        self.total = str(self.l1 + self.l2)
+        print('total: {}'.format(self.total))
         self.next = self._get_next()
+        print('next: {}'.format(self.next))
+        print('val: {}'.format(self.val))
+        return self
 
     def _generate_integer(self, l: ListNode):
         l_string = ''
@@ -21,10 +28,19 @@ class Solution:
         return int(l_string)
 
     def _get_next(self):
-        self.val = str(self.total)[-1]
-        if len(str(self.total)) 
-        int(str(self.total)[:-1])
-        return True
+        len_total = len(self.total)
+        if len_total != 0:
+            print('if\'d')
+            print('val 01: {}'.format(self.val))
+            print('total 01: {}'.format(self.total))
+            self.total = self.total[1:len_total]
+            self.val = self.total[-1]
+            print('total 02: {}'.format(self.total))
+            print('val 02: {}'.format(self.val))
+            return self
+        else:
+            print('else\'d')
+            return None
 
 
 l1 = ListNode(2)
@@ -38,5 +54,8 @@ l2.next.next = ListNode(4)
 result = Solution().addTwoNumbers(l1, l2)
 while result:
     print(result.val)
+    print('sleeping 3s')
+    sleep(3)
     result = result.next
+    
 # 7 0 8
