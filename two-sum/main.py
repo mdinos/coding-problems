bb51b6a4071d08872ef9328586dc1efdcf172a34
mdinos <32902031+mdinos@users.noreply.github.com>
2019-08-03 01:23:54 +0100

    [002] Allow for duplicate values.

--- two-sum/main.py
+++ two-sum/main.py
@@ -1,9 +1,9 @@
 def two_sum(xs, target_value):
     for i, x in enumerate(xs):
-        combinations = [x + y for y in xs if xs.index(y) != i]
+        combinations = [x + y for j, y in enumerate(xs) if j != i]
         print('combinations: {}'.format(combinations))
         if target_value in combinations:
             return True
     return False
-    
+
 print(two_sum([7, 1, -3, 2, 4], 5))
